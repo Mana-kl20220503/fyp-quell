5c56505c7147f8e7fc7f42223a1fe5bde188fdd8
Mana-kl20220503 <mn.kumana@gmail.com>
2024-03-22 04:25:07 +0900

    Initial Commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup>
+
+</script>
+
+<template>
+  <main>
+    <div>Heloooo</div>
+  </main>
+</template>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+    <div>this is login dayo
+    </div>
+  </main>
+</template>
