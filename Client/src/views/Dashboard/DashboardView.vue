6db16dd407924cd86eef53fabd5316b616dff5f9
Mana-kl20220503 <mn.kumana@gmail.com>
2024-05-01 12:50:34 +0900

    [Added] My page/Dashboard page rough design -need to improve

--- Client/src/views/Dashboard/DashboardView.vue
+++ Client/src/views/Dashboard/DashboardView.vue
@@ -1,8 +1,90 @@
-<script setup></script>
-
 <template>
-  <main>
-    <TheWelcome />
-    <div>this is dashborad dayo !!</div>
-  </main>
+  <div class="max-w-4xl mx-auto p-5 bg-white shadow-lg rounded">
+    <div class="text-center">
+      <img
+        :src="user.profileImage"
+        alt="User Image"
+        class="w-20 h-20 rounded-full mx-auto"
+      />
+      <h2 class="text-lg font-bold">{{ user.name }}</h2>
+      <p class="italic">{{ user.reasonToQuit }}</p>
+    </div>
+
+    <div class="my-6">
+      <h3 class="text-xl font-bold text-center mb-4">Today's Vape Log</h3>
+      <div class="flex justify-around items-center">
+        <img
+          v-if="isAllZero"
+          src="/assets/cracker.png"
+          alt="Cracker Left"
+          class="w-12 h-auto"
+        />
+        <div class="flex-grow">
+          <div class="bg-red-400 text-white p-4 rounded-lg shadow-md mx-2">
+            Puffs Taken: {{ vapeLog.puffs }}
+          </div>
+          <div class="bg-blue-400 text-white p-4 rounded-lg shadow-md mx-2">
+            Nicotine Intake: {{ vapeLog.nicotine }} mg
+          </div>
+          <div class="bg-yellow-400 text-white p-4 rounded-lg shadow-md mx-2">
+            Money Spent: £{{ vapeLog.money }}
+          </div>
+        </div>
+        <img
+          v-if="isAllZero"
+          src="/assets/cracker_right.png"
+          alt="Cracker Right"
+          class="w-12 h-auto"
+        />
+      </div>
+    </div>
+
+    <div class="my-6">
+      <h3 class="text-xl font-bold text-center mb-4">Health Info</h3>
+      <div @click="goToArticle" class="cursor-pointer">
+        <img
+          src="/assets/vape1.jpg"
+          alt="Health Info"
+          class="mx-auto w-20 h-auto"
+        />
+        <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry...
+        </p>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script setup>
+import { ref, computed } from 'vue';
+import { useRouter } from 'vue-router';
+
+const user = ref({
+  name: 'User Name',
+  profileImage: '/assets/User1.png',
+  reasonToQuit: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+});
+
+const vapeLog = ref({
+  puffs: 0,
+  nicotine: 0,
+  money: 0,
+});
+
+const isAllZero = computed(() => {
+  return (
+    vapeLog.value.puffs === 0 &&
+    vapeLog.value.nicotine === 0 &&
+    vapeLog.value.money === 0
+  );
+});
+
+const router = useRouter();
+
+function goToArticle() {
+  router.push('/health/article/1');
+}
+</script>
+
+<style scoped></style>
--- Client/src/views/Dashboard/MyPageView.vue
+++ Client/src/views/Dashboard/MyPageView.vue
@@ -1,8 +1,86 @@
-<script setup></script>
-
 <template>
-  <main>
-    <TheWelcome />
-    <div>this is My page dayo !!</div>
-  </main>
+  <div class="max-w-4xl mx-auto my-10 p-5 bg-white shadow-lg rounded">
+    <h1 class="text-2xl font-ultra mb-6 text-center">My Page</h1>
+    <div class="space-y-4">
+      <div
+        v-for="field in fields"
+        :key="field.key"
+        class="flex items-center space-x-4"
+      >
+        <label class="w-48">{{ field.label }}</label>
+        <div v-if="field.editing" :class="{ 'flex-1': field.type !== 'file' }">
+          <input
+            v-model="field.value"
+            :type="field.type"
+            class="border-2 border-black p-2 rounded w-full"
+          />
+        </div>
+        <div v-else :class="{ 'flex-1': field.type !== 'file' }">
+          <span
+            class="border-2 border-black p-2 rounded inline-block min-w-full"
+            >{{ field.value }}</span
+          >
+        </div>
+        <button
+          @click="toggleEdit(field)"
+          class="bg-custom-red hover:bg-custom-blue text-white font-bold py-1 px-4 rounded"
+        >
+          {{ field.editing ? 'Confirm' : 'Edit' }}
+        </button>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script setup>
+import { ref } from 'vue';
+
+const fields = ref([
+  {
+    key: 'username',
+    label: 'User Name',
+    value: 'sampleuser',
+    editing: false,
+    type: 'text',
+  },
+  {
+    key: 'profilePicture',
+    label: 'Profile Picture',
+    value: '',
+    editing: false,
+    type: 'file',
+  },
+  {
+    key: 'email',
+    label: 'Email',
+    value: 'sample@gmail.com',
+    editing: false,
+    type: 'email',
+  },
+  {
+    key: 'password',
+    label: 'Password',
+    value: 'password',
+    editing: false,
+    type: 'password',
+  },
+  {
+    key: 'reason',
+    label: 'Reason to Quit Vape',
+    value: 'Health Concerns',
+    editing: false,
+    type: 'text',
+  },
+]);
+
+function toggleEdit(field) {
+  field.editing = !field.editing;
+}
+</script>
+
+<style scoped>
+input[type='file'] {
+  border: none;
+  background: none;
+}
+</style>
